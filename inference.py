4c93f8ce4f581667528bf1cf02418da289fbd25b
Khaled Yasser <khaledabozaud41@gmail.com>
2026-01-19 01:23:05 +0200

    Update inference.py

--- inference.py
+++ inference.py
@@ -2,26 +2,40 @@ import cv2
 import torch
 import numpy as np
 import segmentation_models_pytorch as smp
+from huggingface_hub import hf_hub_download
 
-# ===== Constants =====
 IMG_SIZE = 352
 THRESHOLD = 0.5
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-# ===== Load model =====
-def load_model(model_path):
+HF_REPO_ID = "khaled314/capsule-segmentation-model"
+HF_MODEL_FILE = "best_model.pth"
+
+
+def download_model():
+    model_path = hf_hub_download(
+        repo_id=HF_REPO_ID,
+        filename=HF_MODEL_FILE
+    )
+    return model_path
+
+def load_model():
+    model_path = download_model()
+
     model = smp.UnetPlusPlus(
         encoder_name="resnet34",
         encoder_weights=None,
         in_channels=3,
         classes=1
     )
+
     model.load_state_dict(torch.load(model_path, map_location=DEVICE))
     model.to(DEVICE)
     model.eval()
+
     return model
 
-# ===== Inference function =====
+
 def run_segmentation(model, image_path, output_path):
     img = cv2.imread(image_path)
 
@@ -32,23 +46,18 @@ def run_segmentation(model, image_path, output_path):
     if len(img.shape) == 2:
         img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 
-    # Save original size
     h, w = img.shape[:2]
 
-    # Preprocess
+
     x = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
     x = x / 255.0
     x = torch.tensor(x).permute(2, 0, 1).unsqueeze(0).float().to(DEVICE)
 
-    # Inference
     with torch.no_grad():
         pred = torch.sigmoid(model(x))[0, 0].cpu().numpy()
 
-    # Threshold
     pred = (pred > THRESHOLD).astype("uint8") * 255
 
-    # Resize back to original size
     pred = cv2.resize(pred, (w, h), interpolation=cv2.INTER_NEAREST)
 
-    # Save mask
     cv2.imwrite(output_path, pred)
